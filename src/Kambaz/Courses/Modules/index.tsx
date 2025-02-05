1206b525df31e442ed6ea40f0b1c53802cca9765
celiaburrington <burr.celia@gmail.com>
2025-02-05 17:18:00 -0500

    add missing buttons to module component

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,7 +1,12 @@
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
+      <button id="wd-collapse">Collapse All</button> 
+      <button id="wd-view-progress">View Progress</button>
+      <select id="wd-publish">
+        <option value="PUBLISH">Publish All</option>
+      </select>
+      <button id="wd-add-module">+ Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
           <div className="wd-title">Week 1</div>
