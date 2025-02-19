a235dd59019773fc11e119f126cc6bb9f46da362
celiaburrington <burr.celia@gmail.com>
2025-02-19 17:52:25 -0500

    complete lab 3 exercises

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,26 +1,37 @@
 import { Nav } from "react-bootstrap";
+import { useLocation } from "react-router";
 
 export default function TOC() {
+  const { pathname } = useLocation();
   return (
     <Nav variant="pills">
-     <Nav.Item>
-       <Nav.Link href="#/Labs">Labs</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="https://github.com/celiaburrington">My GitHub</Nav.Link>
-     </Nav.Item>
-   </Nav>
+      <Nav.Item>
+        {/* made Labs pill never active because no Labs page, default is Lab1 */}
+        <Nav.Link href="#/Labs" active={false}>
+          Labs
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab1" active={pathname.includes("Lab1")}>
+          Lab 1
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab2" active={pathname.includes("Lab2")}>
+          Lab 2
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab3" active={pathname.includes("Lab3")}>
+          Lab 3
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://github.com/celiaburrington">My GitHub</Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
