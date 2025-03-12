57e12c9a3bd3394a68df6894eb1ef5ae4efb3547
celiaburrington <145168676+celiaburrington@users.noreply.github.com>
2025-03-12 08:46:15 -0400

    Assignment 3 (#3)
    * complete lab 3 exercises
    
    * complete kambaz a3 implementation

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,181 +1,47 @@
 import { Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router";
+import * as db from "./Database";
 
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+      <h2 id="wd-dashboard-published">
+        Published Courses ({courses.length})
+      </h2>{" "}
+      <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/reactjs.png"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS1234 React JS
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Full Stack software developer
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1221/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/books.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    ENGW1221 Advanced Writing
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Interdisciplinary Advanced Writing
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/3650/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/oldcomputer.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS3650 Systems
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Computer Systems and Assembly
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/3200/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/databases.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS3200 Database Design
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Introduction to Databases
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/3700/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/networks.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS3700 Networks
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Networks and Distributed Systems
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/4530/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/engineering.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS4530 Software Engineering
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Fundamentals of Software Engineering
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/2321/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/math.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    MATH2321 Calculus 3
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Calculus 3 for Science/Engineering
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
+          {courses.map((course) => (
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  to={`/Kambaz/Courses/${course._id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <Card.Img
+                    src={`images/${course.iconImage}`}
+                    variant="top"
+                    width="100%"
+                    height={160}
+                  />
+                  <Card.Body className="card-body">
+                    <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      {course.name}{" "}
+                    </Card.Title>
+                    <Card.Text
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      {course.description}{" "}
+                    </Card.Text>
+                    <Button variant="primary"> Go </Button>
+                  </Card.Body>
+                </Link>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </div>
     </div>
---
+++ src/Kambaz/Database/index.ts
@@ -0,0 +1,7 @@
+import assignments from "./assignments.json";
+import courses from "./courses.json";
+import modules from "./modules.json";
+import users from "./users.json";
+import enrollments from "./enrollments.json";
+
+export { courses, modules, assignments, users, enrollments };
