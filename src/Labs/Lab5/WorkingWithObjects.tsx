6cd34e99b823be3b40cc9e4dae57061d72e0c7f2
celiaburrington <burr.celia@gmail.com>
2025-04-02 18:42:39 -0400

    complete lab 5 exercises

---
+++ src/Labs/Lab5/WorkingWithObjects.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { FormControl } from "react-bootstrap";
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export default function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const ASSIGNMENT_API_URL = `${REMOTE_SERVER}/lab5/assignment`;
+
+  return (
+    <div id="wd-working-with-objects">
+      <h3>Working With Objects</h3>
+      <h4>Modifying Properties</h4>
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/title/${assignment.title}`}
+      >
+        Update Title
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-title"
+        defaultValue={assignment.title}
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+      />
+      <br />
+      <a
+        id="wd-update-assignment-score"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/score/${assignment.score}`}
+      >
+        Update Score
+      </a>
+      <FormControl
+        type="number"
+        className="w-25"
+        id="wd-assignment-score"
+        defaultValue={assignment.score}
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: parseInt(e.target.value) })
+        }
+      />
+      <br />
+      <a
+        id="wd-update-assignment-completed"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/completed/${assignment.completed}`}
+      >
+        Update Completion
+      </a>
+      <input
+        type="checkbox"
+        id="wd-assignment-completed"
+        checked={assignment.completed}
+        onChange={() =>
+          setAssignment({ ...assignment, completed: !assignment.completed })
+        }
+      />
+      <hr />
+      <h4>Retrieving Objects</h4>
+      <a
+        id="wd-retrieve-assignments"
+        className="btn btn-primary me-2"
+        href={`${REMOTE_SERVER}/lab5/assignment`}
+      >
+        Get Assignment
+      </a>
+      <a
+        id="wd-retrieve-module"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/module`}
+      >
+        Get Module
+      </a>
+      <hr />
+      <h4>Retrieving Properties</h4>
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary me-2"
+        href={`${REMOTE_SERVER}/lab5/assignment/title`}
+      >
+        Get Assignment Title
+      </a>
+      <a
+        id="wd-retrieve-module-name"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/module/name`}
+      >
+        Get Module Name
+      </a>
+      <hr />
+    </div>
+  );
+}
